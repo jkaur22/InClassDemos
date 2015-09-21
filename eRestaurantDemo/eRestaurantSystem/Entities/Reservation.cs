99e5d9c8b8475c75b70474e2e2485f0b82c2336b
Jaspreet <jkaur22@WA302-05.nait.ca>
2015-09-21 17:07:22 -0600

    Setup of Component library with Entity Samples

---
+++ eRestaurantDemo/eRestaurantSystem/Entities/Reservation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+#region Additional Namespaces
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace eRestaurantSystem.Entities
+{
+   public class Reservation
+    {
+       [Key]
+       public int ReservationID { get; set; }
+       [Required]
+       [StringLength(40)]
+       public string CustomerName { get; set; }
+       public DateTime ReservationDate { get; set; }
+       [Range(1,16,ErrorMessage="Party size is limited to 1 to 16.")]
+       public int NumberInParty { get; set; }
+       [StringLength(15)]
+       public string ContactPhone { get; set; }
+       [Required,StringLength(1,MinimumLength=1)]
+       public string ReservationStatus { get; set; }
+       [StringLength(1)]
+       public string EventCode { get; set; }
+
+       //Navigation property(s)
+       public virtual SpecialEvent Event { get; set; }
+       //the reservations table is a many to many relationship 
+       //to tables table.
+       //the Sql Reservations table resolves this problem, however 
+       //Reservation tables holds only a compund primary key
+       //We will not create a reservations table entity in our project.
+       //but handle it via navigation mapping.
+       //Therefore we will place a ICollection property in this entity
+       //refering to the Tables table
+
+       public virtual ICollection<Table> Tables { get; set; }
+
+
+    }
+}
---
+++ eRestaurantDemo/eRestaurantSystem/Entities/Table.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+#region Additional Namespaces
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace eRestaurantSystem.Entities
+{
+  public   class Table
+    {
+
+      [Key]
+      public int TableID { get; set; }
+      [Required,Range(1,25)]
+      public byte TableNumber { get; set; }
+      public bool Smoking { get; set; }
+      public int Capacity { get; set; }
+      public bool Available { get; set; }
+
+      //Navigation Property
+      //the reservations table is a many to many relationship 
+      //to tables table.
+      //the Sql Reservations table resolves this problem, however 
+      //Reservation tables holds only a compund primary key
+      //We will not create a reservations table entity in our project.
+      //but handle it via navigation mapping.
+      //Therefore we will place a ICollection property in this entity
+      //refering to the Reservation table
+
+      public virtual ICollection<Reservation> Reservations { get; set; }
+
+      public Table()
+      {
+          Available = true;
+      }
+    }
+}
